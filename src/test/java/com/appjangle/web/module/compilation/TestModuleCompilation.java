02791abf8d92af747fda43481c2bf748cf54ab90
mxro <mxro@nowhere.com>
2014-10-08 09:33:02 +1100

    Creating project for module compilation Autocommit Wed Oct 08 09:33:02 EST 2014

--- src/test/java/com/appjangle/web/module/compilation/TestModuleCompilation.java
+++ src/test/java/com/appjangle/web/module/compilation/TestModuleCompilation.java
@@ -1,5 +1,14 @@
 package com.appjangle.web.module.compilation;
 
+import org.junit.Test;
+
+import com.appjangle.qunit.QUnit;
+
 public class TestModuleCompilation {
 
+	@Test
+	public void test() {
+		QUnit.run(this, "qunit_module_compile.html");
+	}
+	
 }
